24f9e576251b01b0632beb6723c4837e318b72d7
Arthur Sacramento <sacrarthut06@gmail.com>
2025-11-19 19:41:41 -0300

    Criação de estrutura  Cliente

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct clientes
+{
+ int id;
+ char nome [30];
+ char telefone [12];   
+ char email [50];
+ char empresa [50];
+ char departamento [30];
+
+
+}CLIENTES;
 
 int main (){
     
